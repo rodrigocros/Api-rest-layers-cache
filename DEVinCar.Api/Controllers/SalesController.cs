6396f1545232a118e4b78a88f18ee3807923e421
Rodrigo Rosa <100646597+rodrigocros@users.noreply.github.com>
2022-10-03 23:24:49 -0300

    fix : Endpoints com erros

--- DEVinCar.Api/Controllers/SalesController.cs
+++ DEVinCar.Api/Controllers/SalesController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using DEVinCar.Domain.ViewModels;
 using DEVinCar.Domain.Interfaces.Services;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DEVinCar.Api.Controllers;
 
@@ -13,13 +14,19 @@ namespace DEVinCar.Api.Controllers;
 [Route("api/sales")]
 public class SalesController : ControllerBase
 {
+    private readonly IDeliveryService _deliveryService;
+    private readonly ISaleCarService _saleCarService;
+    private readonly ICarService _carService;
     private readonly ISaleService _saleservice;
     private readonly IMapper _mapper;
 
-    public SalesController(ISaleService saleservice, IMapper mapper)
+    public SalesController(ISaleService saleservice, IMapper mapper, ICarService carService, ISaleCarService saleCarService, IDeliveryService deliveryService)
     {
         _saleservice = saleservice;
         _mapper = mapper;
+        _carService = carService;
+        _saleCarService = saleCarService;
+        _deliveryService = deliveryService;
 
     }
 
@@ -34,154 +41,171 @@ public class SalesController : ControllerBase
         return Ok(sales);
     }
 
-    // [Authorize(Roles = "Gerente")]
-    // [HttpPost("{saleId}/item")]
-    // public ActionResult<SaleCar> PostSale(
-    //    [FromBody] SaleCarDTO body,
-    //    [FromRoute] int saleId
-    //    )
-    // {
-    //     decimal unitPrice;
+    [Authorize(Roles = "Gerente")]
+    [HttpPost("{saleId}/item")]
+    public ActionResult<SaleCar> PostSale(
+       [FromBody] SaleCarDTO body,
+       [FromRoute] int saleId
+       )
+    {
+        decimal unitPrice;
+        var carDTO = _carService.GetById(body.CarId);
+        var carSALE = _saleservice.GetById(body.SaleId);
 
-    //     if (_context.Cars.Any(c => c.Id == body.CarId) && _context.Sales.Any(s => s.Id == body.SaleId))
-    //     {
-    //         if (body.CarId == 0) return BadRequest();
+        // if (_context.Cars.Any(c => c.Id == body.CarId) && _context.Sales.Any(s => s.Id == body.SaleId))
+        if(carDTO != null && carSALE != null)
+        {
+            if (body.CarId == 0) return BadRequest();
 
-    //         if (body.UnitPrice <= 0 || body.Amount <= 0) return BadRequest();
+            if (body.UnitPrice <= 0 || body.Amount <= 0) return BadRequest();
 
-    //         if (body.UnitPrice == null) unitPrice = _context.Cars.Find(body.CarId).SuggestedPrice;
+            if (body.UnitPrice == null) unitPrice =  carDTO.SuggestedPrice;
 
-    //         else unitPrice = (decimal)body.UnitPrice;
+            else unitPrice = (decimal)body.UnitPrice;
 
-    //         if (body.Amount == null) body.Amount = 1;
+            if (body.Amount == null) body.Amount = 1;
 
-    //         var saleCar = new SaleCar
-    //         {
-    //             Id = saleId,
-    //             Amount = body.Amount,
-    //             CarId = body.CarId,
-    //             UnitPrice = unitPrice,
-    //             SaleId = saleId
-    //         };
+            var saleCar = new SaleCar
+            {
+                Id = saleId,
+                Amount = body.Amount,
+                CarId = body.CarId,
+                UnitPrice = unitPrice,
+                SaleId = saleId
+            };
 
-    //         _context.SaleCars.Add(saleCar);
-    //         _context.SaveChanges();
-    //         return Created("api/sales/{saleId}/item", body.CarId);
-    //     }
-    //     return NotFound();
-    // }
+            // _context.SaleCars.Add(saleCar);
+            // _context.SaveChanges();
+            _saleCarService.Inserir(saleCar);
+            
+            return Created("api/sales/{saleId}/item", body.CarId);
+        }
+        return NotFound();
+    }
 
-    // [HttpPost("{saleId}/deliver")]
-    // public ActionResult<DeliveryDTO> PostDeliver(
-    //        [FromRoute] int saleId,
-    //        [FromBody] DeliveryDTO body)
-    // {
-    //     if (!body.AddressId.HasValue)
-    //     {
-    //         return BadRequest();
-    //     }
+    [HttpPost("{saleId}/deliver")]
+    public ActionResult<DeliveryDTO> PostDeliver(
+           [FromRoute] int saleId,
+           [FromBody] DeliveryDTO body)
+    {
 
-    //     if (_context.Sales.Find(saleId) == null)
-    //     {
-    //         return NotFound();
-    //     }
+        if (!body.AddressId.HasValue)
+        {
+            return BadRequest();
+        }
 
-    //     if (_context.Sales.Find(body.AddressId) == null)
-    //     {
-    //         return NotFound();
-    //     }
+        // if (_context.Sales.Find(saleId) == null)
+        if(_saleservice.GetById(saleId) == null)
+        {
+            return NotFound();
+        }
 
-    //     var now = DateTime.Now.Date;
-    //     if (body.DeliveryForecast < now)
-    //     {
-    //         return BadRequest();
-    //     }
+        // funcao sem sentido, nao tem adressID no repositorio Sale. Nao entendi.
+        // if (_context.Sales.Find(body.AddressId) == null)
+        // if(_saleservice.GetById(body.AddressId) == null)
+        // {
+        //     return NotFound();
+        // }
 
-    //     if (body.DeliveryForecast == null)
-    //     {
-    //         body.DeliveryForecast = DateTime.Now.AddDays(7);
-    //     }
+        var now = DateTime.Now.Date;
+        if (body.DeliveryForecast < now)
+        {
+            return BadRequest();
+        }
 
-    //     var deliver = new Delivery
-    //     {
-    //         AddressId = (int)body.AddressId,
-    //         SaleId = saleId,
-    //         DeliveryForecast = (DateTime)body.DeliveryForecast
-    //     };
+        if (body.DeliveryForecast == null)
+        {
+            body.DeliveryForecast = DateTime.Now.AddDays(7);
+        }
 
-    //     _context.Deliveries.Add(deliver);
-    //     _context.SaveChanges();
+        var deliver = new Delivery
+        {
+            AddressId = (int)body.AddressId,
+            SaleId = saleId,
+            DeliveryForecast = (DateTime)body.DeliveryForecast
+        };
 
-    //     return Created("{saleId}/deliver", deliver.Id);
-    // }
+        _deliveryService.Inserir(deliver);
+        // _context.Deliveries.Add(deliver);
+        // _context.SaveChanges();
 
-    // [HttpPatch("{saleId}/car/{carId}/amount/{amount}")]
-    // public ActionResult<SaleCar> Patch(
-    //         [FromRoute] int saleId,
-    //         [FromRoute] int carId,
-    //         [FromRoute] int amount
-    //         )
-    // {
-    //     var carSaleId = _context.Sales.Find(saleId);
-    //     var carID = _context.SaleCars.Find(carId);
+        return Created("{saleId}/deliver", deliver.Id);
+    }
 
-    //     if (carSaleId == null || carID == null)
-    //     {
-    //         return NotFound();
-    //     }
+    [HttpPatch("{saleId}/car/{carId}/amount/{amount}")]
+    public ActionResult<SaleCar> Patch(
+            [FromRoute] int saleId,
+            [FromRoute] int carId,
+            [FromRoute] int amount
+            )
+    {
+        var carSaleId = _saleservice.GetById(saleId);
+        // var carSaleId = _context.Sales.Find(saleId);
+        var carID = _saleCarService.GetById(carId);
+        // var carID = _context.SaleCars.Find(carId);
 
-    //     if (amount <= 0)
-    //     {
-    //         return BadRequest();
-    //     }
+        if (carSaleId == null || carID == null)
+        {
+            return NotFound();
+        }
 
-    //     try
-    //     {
-    //         carID.Amount = amount;
-    //         carID.Amount = amount;
-    //         _context.SaleCars.Update(carID);
-    //         _context.SaveChanges();
-    //         return NoContent();
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         return BadRequest();
-    //     }
-    // }
+        if (amount <= 0)
+        {
+            return BadRequest();
+        }
 
-    // [HttpPatch("{saleId}/car/{carId}/price/{unitPrice}")]
-    // public ActionResult<SaleCar> Patch(
-    //        [FromRoute] int saleId,
-    //        [FromRoute] int carId,
-    //        [FromRoute] decimal unitPrice
-    //        )
-    // {
-    //     var carSaleId = _context.Sales.Find(saleId);
-    //     var carID = _context.SaleCars.Find(carId);
+        try
+        {
+            carID.Amount = amount;
+            carID.Amount = amount;
+            // _context.SaleCars.Update(carID);
+            // _context.SaveChanges();
+            _saleCarService.Inserir(carID);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex);
+        }
+    }
 
-    //     if (carSaleId == null || carID == null)
-    //     {
-    //         return NotFound();
-    //     }
+    [HttpPatch("{saleId}/car/{carId}/price/{unitPrice}")]
+    public ActionResult<SaleCar> Patch(
+           [FromRoute] int saleId,
+           [FromRoute] int carId,
+           [FromRoute] decimal unitPrice
+           )
+    {
+        var carSaleId = _saleservice.GetById(saleId);
+        // var carSaleId = _context.Sales.Find(saleId);
+        // var carID = _context.SaleCars.Find(carId);
+        var carID = _saleCarService.GetById(carId);
 
-    //     if (carID.UnitPrice <= 0)
-    //     {
-    //         return BadRequest();
-    //     }
 
-    //     try
-    //     {
-    //         carID.UnitPrice = unitPrice;
-    //         _context.SaleCars.Update(carID);
-    //         _context.SaveChanges();
-    //         return NoContent();
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         return BadRequest(ex);
-    //     }
+        if (carSaleId == null || carID == null)
+        {
+            return NotFound();
+        }
 
-    // }
+        if (carID.UnitPrice <= 0)
+        {
+            return BadRequest();
+        }
+
+        try
+        {
+            carID.UnitPrice = unitPrice;
+            // _context.SaleCars.Update(carID);
+            // _context.SaveChanges();
+            _saleCarService.Inserir(carID);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex);
+        }
+
+    }
 
 }
 
